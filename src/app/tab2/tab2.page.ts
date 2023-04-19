270c3a3aa6e2e26beaab8e1b01d94f245091e937
saratiedt <saracristinetiedt@gmail.com>
2023-04-19 14:26:06 -0300

    Added form

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,31 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { BaseService } from '../shared/services/base.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, ReactiveFormsModule],
 })
-export class Tab2Page {
-  constructor(private service: BaseService, private _sanitizer: DomSanitizer) {}
+export class Tab2Page implements OnInit {
+  ionicForm!: FormGroup;
+  base64Image: any;
+  convertedImage: any;
 
- base64Image: any;
- convertedImage: any;
+  constructor(
+    private service: BaseService,
+    private _sanitizer: DomSanitizer,
+    public formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.ionicForm = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.minLength(2)]],
+      subtitle: ['', [Validators.required, Validators.minLength(2)]],
+      description: ['', [Validators.required, Validators.minLength(2)]],
+    });
+  }
 
   loadImage(event: any) {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
-       this.base64Image = reader.result;
+      this.base64Image = reader.result;
 
-       // convert image to display
-       this.convertedImage = this._sanitizer.bypassSecurityTrustResourceUrl(this.base64Image);
+      // convert image to display
+      this.convertedImage = this._sanitizer.bypassSecurityTrustResourceUrl(
+        this.base64Image
+      );
     };
+  }
+
+  submitForm() {
+    console.log('bla', this.ionicForm);
   }
 }
